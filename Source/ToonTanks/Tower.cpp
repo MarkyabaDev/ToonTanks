9d58b1930154b185f58079823a902b85c48722f5
Markus Baars <markus@baars42.de>
2022-01-09 18:48:46 +0100

    Create Tower Class

---
+++ Source/ToonTanks/Tower.cpp
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Tower.h"
+
+#include "Tank.h"
+#include "Kismet/GameplayStatics.h"
+
+void ATower::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	if (Tank)
+	{
+		const float DistanceToTank = FVector::Dist(GetActorLocation(), Tank->GetActorLocation());
+		if(DistanceToTank <= FiringRange)
+		{
+			RotateTurret(Tank->GetActorLocation());	
+		}
+	}
+}
+
+void ATower::BeginPlay()
+{
+	Super::BeginPlay();
+
+	Tank = Cast<ATank>(UGameplayStatics::GetPlayerPawn(this, 0));
+}
+
